db9931f79dfd65af00625c685aba615418614e40
sanath17 <sanath17@users.noreply.github.com>
2017-07-25 00:51:34 -0500

    feat(module-api):movie and userreview added

--- server/MovieworldSpringApi/src/main/java/com/movieworld/MovieworldSpringApi/repository/MovieRepository.java
+++ server/MovieworldSpringApi/src/main/java/com/movieworld/MovieworldSpringApi/repository/MovieRepository.java
@@ -1,5 +1,26 @@
 package com.movieworld.MovieworldSpringApi.repository;
 
-public class MovieRepository {
+import java.util.List;
+
+import com.movieworld.MovieworldSpringApi.entity.Movie;
+
+public interface MovieRepository {
+
+	public  List<Movie> findAll();
+	
+	public  Movie findByTitle(Movie title);
+	
+	public Movie findByGenere(String genere);
+	
+	public Movie findByRatings(int imdb_ratings );
+	
+	public Movie findByType(String type);
+	
+	public Movie create(Movie title);
+	
+	public Movie update(Movie title);
+	
+	public void delete(Movie title);
+
 
 }
--- server/MovieworldSpringApi/src/main/java/com/movieworld/MovieworldSpringApi/service/MovieService.java
+++ server/MovieworldSpringApi/src/main/java/com/movieworld/MovieworldSpringApi/service/MovieService.java
@@ -1,5 +1,26 @@
 package com.movieworld.MovieworldSpringApi.service;
 
-public class MovieService {
+import java.util.List;
 
+import com.movieworld.MovieworldSpringApi.entity.Movie;
+
+public interface MovieService {
+	
+	public  List<Movie> findAll();
+	
+	public  Movie findByTitle(Movie title);
+	
+	public Movie findByGenere(String genere);
+	
+	public Movie findByRatings(int imdb_ratings );
+	
+	public Movie findByType(String type);
+	
+	public Movie create(Movie title);
+	
+	public Movie update(Movie title); 
+	
+	public void delete(Movie title);
+
+	
 }
